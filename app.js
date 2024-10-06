b6e872511f7ac107e73093106559c5a20af3f3d1
laibasami56 <laibasami9876@gmail.com>
2024-10-06 16:19:43 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+// Question # 01 : Write a script to greet your website visitor using JS alert
+// box.
+
+alert("Welcome to our website! We hope you have a great experience.");
+
+// Question # 02 : Write a script to display following message on your web
+// page:
+
+alert("Error! Please Enter a Valid Password");
+
+// Question # 03 : Write a script to display following message on your web
+// page: (Hint : Use line break)
+
+alert("Welcome to JS Land... \n"  + "Happy Coding!");
+
+// Question # 04 : 4. Write a script to display following messages in sequence: 
+
+
+alert("Welcome to JS Land...");
+alert("Happy Coding!\n(Prevent this page from creating additional dialogs.)");
+
+// Question # 05 : Generate the following message through browser’s
+// developer console:
+ 
+alert("Hello... I can run JS  through my web brower's console");
+
+// Question # 06 : Make use of alerts in your new/existing HTML & CSS
+//project.
+
+var button = document.getElementById('submitButton');
+button.onclick = function() {
+    alert("Thank you for your submission!");
+};
+
+//// Question # 07 : 7. Practice placement of <script></script> element in
+// following sections of your project in exercise 6:
+// a. Head
+// b. Body (before your page’s HTML)
+
+// c. Body (inside your page’s HTML)
+// d. Body (after your page’s HTML)
+
+// c. Script inside your page's HTML 
+   
+    
